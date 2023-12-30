49b5070eb6592bd92a1726936c1a921ec73587c1
linus-md <linus.sommer@protonmail.com>
2023-12-30 12:32:18 +0100

    finish chapter 1

--- hello.hs
+++ hello.hs
@@ -5,5 +5,9 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [arg1, arg2] -> putStrLn (arg1 ++ " says hello to " ++ arg2 ++ "!")
-        _ -> putStrLn "Please provide exactly two arguments."
+        [name1, name2, age1, age2] -> do
+            putStrLn (name1 ++ " says hello to " ++ name2 ++ "!")
+            putStrLn (name1 ++ " is " ++ age1 ++ " years old.")
+            putStrLn (name2 ++ " is " ++ age2 ++ " years old.")
+            putStrLn ("Together, they are " ++ show (read age1 + read age2) ++ " years old.")
+        _ -> putStrLn "Please provide exactly four arguments (Two names, followed by two ages.)"
